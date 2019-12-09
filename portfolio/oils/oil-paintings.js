cbbf393726abb6f307c073b76e20f4cd314563e3
dunbar-brett <dunbar.brett@gmail.com>
2019-12-08 21:46:27 -0800

    variety of fixes and portfolio titles and descriptions

--- portfolio/oils/oil-paintings.js
+++ portfolio/oils/oil-paintings.js
@@ -1,47 +1,75 @@
+/***************************************
+ * 
+ * IF YOU'RE ADDING TO THIS LIST
+ * 
+ ***************************************
+ * * Double check the new image height is 989 pixels
+ * * Do not have duplicate IDs
+ * * Don't forget a comma after each object
+ */
+
 export const oils = [
     {
         id: 0,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/dancer-oil.jpg"
     },
     {
         id: 1,
         customClass: "fight-club",
+        title: "title",
+        description: "description",
         filename: "oils/fight-club-oil.jpg"
     },
     {
         id: 2,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/Screen Shot 2019-09-28 at 1.56.49 PM.png"
     },
     {
         id: 3,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/self-portrait-oil-2015.jpg"
     },
     {
         id: 4,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/self-portrait-oil-2016.jpg"
     },
     {
         id: 5,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/skull-oil.png"
     },
     {
         id: 6,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/triptic-1.jpg"
     },
     {
         id: 7,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/triptic-2.jpg"
     },
     {
         id: 8,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "oils/triptic-3.jpg"
     },
 ]
--- portfolio/watercolor/watercolors.js
+++ portfolio/watercolor/watercolors.js
@@ -2,51 +2,71 @@ export const watercolors = [
     {
         id: 0,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/bb-8.jpg"
     },
     {
         id: 1,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/Cheers.jpg"
     },
     {
         id: 2,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/CompasJellyfish1.jpg"
     },
     {
         id: 3,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/Jelly-head.jpg"
     },
     {
         id: 4,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/MoonJellySmall.jpg"
     },
     {
         id: 5,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/MTHood.jpg"
     },
     {
         id: 6,
         customClass: "house",
+        title: "title",
+        description: "description",
         filename: "watercolor/PDXHouse.jpg"
     },
     {
         id: 7,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/squished-face.png"
     },
     {
         id: 8,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/ThreadWatercolorDeer.jpg"
     },
     {
         id: 9,
         customClass: "",
+        title: "title",
+        description: "description",
         filename: "watercolor/zach-attack.jpg"
     },
 ]
