c3feab5e7a8c446074c35be3dfbf032ff13b59dc
dunbar-brett <dunbar.brett@gmail.com>
2019-12-08 17:08:58 -0800

    New first real deployment

---
+++ portfolio/oils/oil-paintings.js
@@ -0,0 +1,47 @@
+export const oils = [
+    {
+        id: 0,
+        customClass: "",
+        filename: "oils/dancer-oil.jpg"
+    },
+    {
+        id: 1,
+        customClass: "fight-club",
+        filename: "oils/fight-club-oil.jpg"
+    },
+    {
+        id: 2,
+        customClass: "",
+        filename: "oils/Screen Shot 2019-09-28 at 1.56.49 PM.png"
+    },
+    {
+        id: 3,
+        customClass: "",
+        filename: "oils/self-portrait-oil-2015.jpg"
+    },
+    {
+        id: 4,
+        customClass: "",
+        filename: "oils/self-portrait-oil-2016.jpg"
+    },
+    {
+        id: 5,
+        customClass: "",
+        filename: "oils/skull-oil.png"
+    },
+    {
+        id: 6,
+        customClass: "",
+        filename: "oils/triptic-1.jpg"
+    },
+    {
+        id: 7,
+        customClass: "",
+        filename: "oils/triptic-2.jpg"
+    },
+    {
+        id: 8,
+        customClass: "",
+        filename: "oils/triptic-3.jpg"
+    },
+]
---
+++ portfolio/watercolor/watercolors.js
@@ -0,0 +1,52 @@
+export const watercolors = [
+    {
+        id: 0,
+        customClass: "",
+        filename: "watercolor/bb-8.jpg"
+    },
+    {
+        id: 1,
+        customClass: "",
+        filename: "watercolor/Cheers.jpg"
+    },
+    {
+        id: 2,
+        customClass: "",
+        filename: "watercolor/CompasJellyfish1.jpg"
+    },
+    {
+        id: 3,
+        customClass: "",
+        filename: "watercolor/Jelly-head.jpg"
+    },
+    {
+        id: 4,
+        customClass: "",
+        filename: "watercolor/MoonJellySmall.jpg"
+    },
+    {
+        id: 5,
+        customClass: "",
+        filename: "watercolor/MTHood.jpg"
+    },
+    {
+        id: 6,
+        customClass: "house",
+        filename: "watercolor/PDXHouse.jpg"
+    },
+    {
+        id: 7,
+        customClass: "",
+        filename: "watercolor/squished-face.png"
+    },
+    {
+        id: 8,
+        customClass: "",
+        filename: "watercolor/ThreadWatercolorDeer.jpg"
+    },
+    {
+        id: 9,
+        customClass: "",
+        filename: "watercolor/zach-attack.jpg"
+    },
+]
