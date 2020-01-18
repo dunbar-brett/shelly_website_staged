44a28eea36d1fbc4694e35a7ed2f76961648f89d
dunbar-brett <dunbar.brett@gmail.com>
2020-01-17 22:49:08 -0800

    some post launch bug fixes
    scroll bar fix  at tablet break points
    made gallery headers smaller
    attempt at og image change
    copy fix on about

--- portfolio/oils/oil-paintings.js
+++ portfolio/oils/oil-paintings.js
@@ -12,64 +12,64 @@ export const oils = [
     {
         id: 0,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "The Dancer",
+        description: "Oil on Canvas 24\"x 30\"",
         filename: "oils/dancer-oil.jpg"
     },
     {
         id: 1,
         customClass: "fight-club",
-        title: "title",
-        description: "description",
+        title: "I See You",
+        description: "Oil on Canvas 18\"x24\"",
         filename: "oils/fight-club-oil.jpg"
     },
     {
         id: 2,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Ericka",
+        description: "Oil on Canvas 16\"x20\"",
         filename: "oils/Screen Shot 2019-09-28 at 1.56.49 PM.png"
     },
     {
         id: 3,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Self Portrait 2015",
+        description: "Oil on Canvas Panel, 20\"x16\"",
         filename: "oils/self-portrait-oil-2015.jpg"
     },
     {
         id: 4,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Self Portrait 2016",
+        description: "Oil on Canvas 20\"x16\"",
         filename: "oils/self-portrait-oil-2016.jpg"
     },
     {
         id: 5,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Skull",
+        description: "Egg Tempera on Clay Board 8\"x10\"",
         filename: "oils/skull-oil.png"
     },
     {
         id: 6,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "There is No I in Threesome: Part 1",
+        description: "Oil on Canvas triptych 24\"x36\"",
         filename: "oils/triptic-1.jpg"
     },
     {
         id: 7,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "There is No I in Threesome: Part 2",
+        description: "Oil on Canvas triptych 24\"x36\"",
         filename: "oils/triptic-2.jpg"
     },
     {
         id: 8,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "There is No I in Threesome: Part 3",
+        description: "Oil on Canvas triptych 24\"x36\"",
         filename: "oils/triptic-3.jpg"
     },
 ]
--- portfolio/watercolor/watercolors.js
+++ portfolio/watercolor/watercolors.js
@@ -2,71 +2,71 @@ export const watercolors = [
     {
         id: 0,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "BB-8",
+        description: "Watercolor 11\"x14\"",
         filename: "watercolor/bb-8.jpg"
     },
     {
         id: 1,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Cheers",
+        description: "Watercolor 10\"x14\"",
         filename: "watercolor/Cheers.jpg"
     },
     {
         id: 2,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Compass Jelly",
+        description: "Watercolor 14k gold thread 3\"x4\"",
         filename: "watercolor/CompasJellyfish1.jpg"
     },
     {
         id: 3,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Jelly",
+        description: "Watercolor 11\"x14\"",
         filename: "watercolor/Jelly-head.jpg"
     },
     {
         id: 4,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Moon Jelly",
+        description: "Watercolor 3\"x4\"",
         filename: "watercolor/MoonJellySmall.jpg"
     },
     {
         id: 5,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Mt. Hood",
+        description: "Watercolor 8\"x10\"",
         filename: "watercolor/MTHood.jpg"
     },
     {
         id: 6,
         customClass: "house",
-        title: "title",
-        description: "description",
+        title: "Home",
+        description: "Watercolor 8\"x10\"",
         filename: "watercolor/PDXHouse.jpg"
     },
     {
         id: 7,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "The Girl",
+        description: "Watercolor, charcoal, color pencil, thread 22\"x22\"",
         filename: "watercolor/squished-face.png"
     },
     {
         id: 8,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Dear",
+        description: "Watercolor 11\"x14\"",
         filename: "watercolor/ThreadWatercolorDeer.jpg"
     },
     {
         id: 9,
         customClass: "",
-        title: "title",
-        description: "description",
+        title: "Zach-Attack",
+        description: "Watercolor 10\"x14\"",
         filename: "watercolor/zach-attack.jpg"
     },
 ]
